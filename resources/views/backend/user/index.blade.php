9b48277e35104bc7c1d39c6a0749ae331e3a5864
Encrypt <christan.jake2025@yahoo.com>
2020-04-21 19:07:40 +0800

    Added vue loop for users list instead of using PHP loop.

--- resources/views/backend/user/index.blade.php
+++ resources/views/backend/user/index.blade.php
@@ -7,27 +7,9 @@
 				<div class="panel-heading"><h4>Users</h4></div>
 
 				<div class="panel-body">
-					<table class="table">
-						<thead>
-							<tr>
-								<th>Name</th>
-								<th>E-mail</th>
-								<th>Date Created</th>
-								<th>Actions</th>
-							</tr>
-						</thead>
-
-						<tbody>
-							@foreach ($users as $user)
-								<tr>
-									<td>{{ $user->name }}</td>
-									<td>{{ $user->email }}</td>
-									<td>{{ date('F d, Y', strtotime($user->created_at)) }}</td>
-									<td></td>
-								</tr>
-							@endforeach
-						</tbody>
-					</table>
+					<div id="app">
+						<user-list :users="{{ json_encode($users) }}"></user-list>
+					</div>
 				</div>
 			</div>
 		</div>
