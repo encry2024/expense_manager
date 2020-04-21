6cc9d06e11fe5d6422079c78f210698cb77d8743
Encrypt <christan.jake2025@yahoo.com>
2020-04-21 17:14:07 +0800

    Added index.blade.php for expense module.

---
+++ resources/views/backend/expense/index.blade.php
@@ -0,0 +1,14 @@
+@extends('backend.layouts.app')
+
+@section('content')
+	<div class="row">
+		<div class="col-lg-12">
+			<div class="panel panel-default">
+				<div class="panel-heading"><h4>Expense</h4></div>
+
+				<div class="panel-body">
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
