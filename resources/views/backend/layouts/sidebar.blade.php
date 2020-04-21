392742cb8bd797e2ddd80c771d206ed3242c0274
Encrypt <christan.jake2025@yahoo.com>
2020-04-21 17:21:19 +0800

    Added links per module.

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -3,7 +3,9 @@
 		<a href="{{ route('backend.dashboard') }}" class="list-group-item list-group-item-action {{ active_class(Active::checkUriPattern('dashboard')) }}">
 			Dashboard
 		</a>
-		<a href="#" class="list-group-item list-group-item-action {{ active_class(Active::checkUriPattern('expenses*')) }}">Expenses</a>
-		<a href="#" class="list-group-item list-group-item-action {{ active_class(Active::checkUriPattern('users*')) }}">Users</a>
+		<a href="{{ route('backend.expense.index') }}" class="list-group-item list-group-item-action {{ active_class(Active::checkUriPattern('expense*')) }}">Expenses</a>
+		@if (Auth::user()->hasRole('administrator'))
+			<a href="{{ route('backend.admin.user.index') }}" class="list-group-item list-group-item-action {{ active_class(Active::checkUriPattern('user*')) }}">Users</a>
+		@endif
 	</div>
 </div>
