e319c3178a5f51caebb4281e531107b68ae097b6
Encrypt <christan.jake2025@yahoo.com>
2020-04-21 17:22:06 +0800

    Added relationship for Expense model.

--- app/Models/User/Traits/Relationship/UserRelationship.php
+++ app/Models/User/Traits/Relationship/UserRelationship.php
@@ -1,10 +1,13 @@
 <?php
 
-
 namespace App\Models\User\Traits\Relationship;
 
+use App\Models\Expense\Expense;
 
 trait UserRelationship
 {
-
+    public function expenses()
+    {
+        $this->hasMany(Expense::class);
+    }
 }
