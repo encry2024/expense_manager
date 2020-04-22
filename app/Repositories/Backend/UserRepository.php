80b68535e4df1fe0620f36a286ae2b89b9b02b5b
Encrypt <christan.jake2025@yahoo.com>
2020-04-22 22:10:41 +0800

    Added update function in user repository.

--- app/Repositories/Backend/UserRepository.php
+++ app/Repositories/Backend/UserRepository.php
@@ -3,11 +3,10 @@
 
 namespace App\Repositories\Backend;
 
-
-use App\Exceptions\GeneralException;
 use App\Models\User\User;
 use App\Repositories\BaseRepository;
 use Illuminate\Pagination\LengthAwarePaginator;
+use DB;
 
 class UserRepository extends BaseRepository
 {
@@ -52,24 +51,57 @@ class UserRepository extends BaseRepository
             ]);
 
             // See if adding any additional permissions
-            if (! isset($data['permissions']) || ! count($data['permissions'])) {
-                $data['permissions'] = [];
+            if (! isset($data['permissionField']) || ! count($data['permissionField'])) {
+                $data['permissionField'] = [];
             }
 
             if ($user) {
                 // User must have at least one role
-                if (! count($data['roles'])) {
+                if (! count($data['roleField'])) {
                     throw new GeneralException('User must have at least one role');
                 }
 
                 // Add selected roles/permissions
-                $user->syncRoles($data['roles']);
-                $user->syncPermissions($data['permissions']);
+                $user->assignRole($data['roleField']);
+                $user->syncPermissions($data['permissionField']);
 
                 return $user;
             }
 
             throw new GeneralException('There was a problem creating this user. Please try again.');
+        });
+    }
+
+    /**
+     * @param \App\Models\Auth\User $user
+     * @param array $data
+     *
+     * @return User
+     *@throws \Exception
+     * @throws \Throwable
+     * @throws GeneralException
+     */
+    public function update(User $user, array $data)
+    {
+        // See if adding any additional permissions
+        if (! isset($data['permissions']) || ! count($data['permissions'])) {
+            $data['permissions'] = [];
+        }
+
+        return DB::transaction(function () use ($user, $data) {
+            if ($user->update([
+                'first_name' => $data['first_name'],
+                'last_name' => $data['last_name'],
+                'email' => $data['email'],
+            ])) {
+
+                $user->syncRoles($data['userRoles']);
+                $user->syncPermissions($data['permissions']);
+
+                return $user;
+            }
+
+            throw new GeneralException(__('exceptions.backend.access.users.update_error'));
         });
     }
 }
