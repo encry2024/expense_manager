18ded78ebaca8015512130dd3e9b07dbaccf3fce
Encrypt <christan.jake2025@yahoo.com>
2020-04-22 21:54:43 +0800

    Updates bootstrap.js, removed unnecessary codes.

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,26 +1,10 @@
+import Vue from 'vue';
+import axios from 'axios';
+import HighchartsVue from 'highcharts-vue'
 
-window._ = require('lodash');
-
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-
-try {
-    window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap-sass');
-} catch (e) {}
-
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
-window.axios = require('axios');
-
+window.Vue = Vue;
+window.axios = axios;
+Vue.use(HighchartsVue);
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
