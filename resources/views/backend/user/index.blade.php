ea1fffc1b49b404631fdb30baec395feab364d23
Encrypt <christan.jake2025@yahoo.com>
2020-04-22 22:20:54 +0800

    Removed unnecessary html codes.

--- resources/views/backend/user/index.blade.php
+++ resources/views/backend/user/index.blade.php
@@ -1,17 +1,5 @@
 @extends('backend.layouts.app')
 
 @section('content')
-	<div class="row">
-		<div class="col-lg-12">
-			<div class="panel panel-default">
-				<div class="panel-heading"><h4>Users</h4></div>
 
-				<div class="panel-body">
-					<div id="app">
-						<user-list :users="{{ json_encode($users) }}"></user-list>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
 @endsection
