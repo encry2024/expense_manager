5c294e4c81952e7f9056ee93c1b4ad44e301426d
Encrypt <christan.jake2025@yahoo.com>
2020-04-22 21:46:52 +0800

    Added routes for listing.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User\User;
 use Illuminate\Http\Request;
 
 /*
@@ -16,3 +17,40 @@ use Illuminate\Http\Request;
 /*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });*/
+
+Route::get('/expenses/list', function () {
+    $expenses = \App\Models\Expense\Expense::all();
+
+    return $expenses;
+});
+
+
+Route::get('/users/list', function () {
+    $users = User::all();
+
+    return $users;
+});
+
+Route::get('/roles/list', function () {
+    $roles = \Spatie\Permission\Models\Role::with('permissions')->get();
+
+    return $roles;
+});
+
+Route::get('/permissions/list', function () {
+    return \App\Models\Permission\Permission::all();
+});
+
+Route::get('/report', function () {
+    $report = [];
+    $categories = \App\Models\Category\Category::with(['expenses'])->get();
+
+    foreach ($categories as $category) {
+        $report[] = [
+            'name' => $category->name,
+            'y' => $category->expenses->sum('amount')
+        ];
+    }
+
+    return json_encode($report);
+});
