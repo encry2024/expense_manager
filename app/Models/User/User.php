d39bb67f9a8588e6cb457c8cd86dd13d5c75ba14
Encrypt <christan.jake2025@yahoo.com>
2020-04-22 22:16:28 +0800

    Added fillables and accessors.

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -25,7 +25,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'first_name', 'last_name', 'email', 'password',
     ];
 
     /**
@@ -37,5 +37,5 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    protected $appends = ['name'];
+    protected $appends = ['name', 'formatted_created_at'];
 }
