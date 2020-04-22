4cdf44cbc2a0cbb77cb7fa0e63ff639393fe7b17
Encrypt <christan.jake2025@yahoo.com>
2020-04-22 21:55:28 +0800

    Added needed routes for CRUD Category.

--- routes/backend/category.php
+++ routes/backend/category.php
@@ -6,6 +6,13 @@ Route::group([
     'namespace' => 'Category'
 ], function () {
 
-    Route::resource('category', 'CategoryController');
+    Route::get('category/create', 'CategoryController@index');
+    Route::get('category', 'CategoryController@index');
+    Route::get('category/list', 'CategoryController@categoryList');
+    Route::post('category/store', 'CategoryController@store');
+    Route::get('category/{category}/edit', 'CategoryController@index');
+    Route::post('category/{category}/update', 'CategoryController@update');
+    Route::get('category/{category}/detail', 'CategoryController@categoryDetail');
+    Route::post('category/{category}/delete', 'CategoryController@destroy');
 
 });
